5a514712bdb25c68be4ee325a7858f4c3e3eb972
Hexactinellida <roshanpanthee020@gmail.com>
2025-01-25 11:37:19 +0545

    FROM FRONTEND GUY

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+// import Disaster from './Pages/Disaster';
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Disaster />
     </div>
   );
 };
